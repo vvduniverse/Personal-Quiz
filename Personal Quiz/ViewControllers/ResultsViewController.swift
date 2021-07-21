35519c2fe0b3e6c51bc9696f183972db067ba829
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-07-21 14:32:03 +0300

    Initial Commit

---
+++ Personal Quiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  Personal Quiz
+//
+//  Created by brubru on 19.07.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встерчающийся тип живтоного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        
+    }
+    
+}
